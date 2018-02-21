efcb2b67e85577858591df512e358181a024f367
Safet Zahirovic <zahirovic.safet@gmail.com>
2018-02-21 14:01:27 +0100

    Oppdatert om nettsiden, startet med forsiden, redigert css.

--- scripts/script.js
+++ scripts/script.js
@@ -41,5 +41,26 @@ function onScroll(event){
     });
 };
 
+var img = new Image,
+    canvas = document.getElementById('gif'),
+    ctx = canvas.getContext('2d');
+
+/// change to this color
+ctx.fillStyle = '#00c';
+
+/// load image, when ready render it with new color
+img.onload = render;
+img.src = 'test1.gif';
+
+function render() {
+    /// this composite mode clears the canvas as well
+    ctx.globalCompositeOperation = 'copy';
+    ctx.drawImage(img, 0, 0);
+
+    /// this mode fills in whatever, in the image
+    ctx.globalCompositeOperation = 'source-in';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 
 
