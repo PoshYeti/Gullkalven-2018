4b1e0a526dfc8a115865150c4078e9bdd0479bbe
Safet Zahirovic <zahirovic.safet@gmail.com>
2018-02-22 23:22:56 +0100

    Endret navbaren bakgrunn

--- scripts/backtotop.js
+++ scripts/backtotop.js
@@ -3,12 +3,30 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) { 
         document.getElementById("top-btn").style.display = "block";
+         
+        
     } else {
         document.getElementById("top-btn").style.display = "none";
+        
     }
-}
+};
+
+
 
 function topFunction() {
     $("html, body").animate({ scrollTop: 0 }, 600); 
     return false; 
-}
+};
+
+$(document).ready( function() {
+    $("#background-top-bar").hide(); //hide your div initially
+    var topOfOthDiv = $("#show").offset().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#background-top-bar").show(); //reached the desired point -- show div
+        }
+        else{
+            $("#background-top-bar").hide(); 
+        }
+    });
+});
