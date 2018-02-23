7cf5d6ee0d8c7fffc678edd608ba6e98ec7a5ec3
Safet Zahirovic <zahirovic.safet@gmail.com>
2018-02-23 11:42:05 +0100

    Fikset navbar med fade in og out animasjon

--- scripts/backtotop.js
+++ scripts/backtotop.js
@@ -18,28 +18,14 @@ function topFunction() {
     return false; 
 };
 
-$(document).ready( function() {
-    $("#background-top-bar").hide(); //hide your div initially
-    var topOfOthDiv = $("#show").offset().top;
-    var bottomOfOthDiv= $("#endShow").pageTop + $(window).height();
-    $(window).scroll(function() {
-        
-        
-        if($(window).scrollTop() < topOfOthDiv) {
-            $("#background-top-bar").fadeOut("fast");
-            $("#background-top-bar").hide();  
-           
-            //reached the desired point -- show div
-        } 
-
-        else{
-             $("#background-top-bar").fadeIn("fast");//scrolled past the other div?
-            $("#background-top-bar").show();
-            
-        }
-    });
-   
-        
-    
-});
+$("#show").next().height($("#show").height())
+$(window).scroll(function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > 200) {
+        $('#background-top-bar').css('opacity',1);
+    }
+    else {
+        $('#background-top-bar').css('opacity',0+scrollTop/100);
+    }
+}).scroll()
 
